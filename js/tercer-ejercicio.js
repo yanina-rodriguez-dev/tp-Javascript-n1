770b32154840ee4795f0bab834354b383f898216
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-03-07 08:04:27 -0300

    tercer ejercicio

---
+++ js/tercer-ejercicio.js
@@ -0,0 +1,7 @@
+//Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+let num1=(parseInt('3'));
+let num2=(parseInt('5'));
+ 
+let suma=(parseInt('3'))+(parseInt('5'));
+document.write('El resultado de la suma es '+ suma)
+
